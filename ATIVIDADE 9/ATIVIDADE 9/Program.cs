36a8d2a976405236c858f00106a9ba5723f6f360
pinafomariaclara-glitch <pinafomariaclara@gmail.com>
2025-10-02 17:13:51 -0300

    Add files via upload

---
+++ ATIVIDADE 9/ATIVIDADE 9/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.Write("Digite a quantidade de maçãs compradas: ");
+
+int quantidade = int.Parse(Console.ReadLine());
+
+// se forem menos de 12 maçãs, preço é 1,30, Caso contrário, 1,00.
+decimal precoUnitario * quantidade;
+decimal total = precoUnitario * quantidade;
+
+Console.WriteLine();
+Console.WriteLine($"quantidade: {quantidade}");
+Console.WriteLine($"preço unitario: {precoUnitario.ToString("C")}");
+Console.WriteLine($"Custo total de compra: {total.ToString("C")}");
+
+
+Console.WriteLine("/nPressione Enter para sair...");
+Console.ReadLine();

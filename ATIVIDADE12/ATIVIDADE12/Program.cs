860d5b14b548a46850530f458cbd9414f86aa358
pinafomariaclara-glitch <pinafomariaclara@gmail.com>
2025-10-02 17:16:41 -0300

    Add files via upload

---
+++ ATIVIDADE12/ATIVIDADE12/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.Write("Digite o primeiro valor: ");
+double a = double.Parse(Console.ReadLine() ?? "0");
+
+Console.Write(a"Digite o segundo valor: ");
+double b = double.Parse(Console.ReadLine() ?? "0");
+
+double maior = (a > b) ? a : b;
+
+double maior = ($"O maior valor é: {maior}");
+Console.WriteLine(maior"Pressione Enter para sair...");
+Console.WriteLine(maior);

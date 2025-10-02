a26bb1d9e3fce3b8a066de7d6397dfaaa32270bb
pinafomariaclara-glitch <pinafomariaclara@gmail.com>
2025-10-02 17:17:20 -0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿
+Console.Write("Digite um valor: ");                                                                          
+double valor = double.Parse(Console.ReadLine());
+
+if (valor > 10)
+    Console.WriteLine("É MAIOR QUE 10!");
+
+Console.WriteLine("NÃÕ É MAIOR QUE 10!");

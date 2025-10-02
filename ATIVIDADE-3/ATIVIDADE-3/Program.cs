08ef75cb3cadce54c70e657734f932ef4f9e923a
pinafomariaclara-glitch <pinafomariaclara@gmail.com>
2025-10-02 17:14:25 -0300

    Add files via upload

---
+++ ATIVIDADE-3/ATIVIDADE-3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Digite a base do retangulo: ");
+double b = double.Parse(Console.ReadLine());
+
+Console.Write(b"Digite a altura do retangulo: ");
+double h = double.Parse(Console.ReadLine());
+
+double area = b * h;
+
+Console.WriteLine($"A area do retangulo é´: {area}");

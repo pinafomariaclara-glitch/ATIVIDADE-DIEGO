e12c4e3b7a706370c958a946c955961595d14f61
pinafomariaclara-glitch <pinafomariaclara@gmail.com>
2025-10-02 17:13:22 -0300

    Add files via upload

---
+++ atividade 1/atividade 1/Program.cs
@@ -0,0 +1,13 @@
+﻿//declaração de variá´veis
+
+int ValorA = 10;
+int ValorB = 20;
+int auxiliar = 0;
+
+Console.WriteLine($"o valor ANTERIOR de A = {ValorA}, B = }ValorB}");
+
+auxiliar = ValorA;
+ValorA = ValorB;
+ValorB = auxiliar;
+
+Console.WriteLine($"o valor ATUAL de: A = {ValorA}, B ={ValorB}");

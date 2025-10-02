70c3c297baf9285e873c5901558013275b300cb3
pinafomariaclara-glitch <pinafomariaclara@gmail.com>
2025-10-02 17:14:52 -0300

    Add files via upload

---
+++ atividade4/atividade4/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.Write("Digite sua idade em anos: ");
+int anos = int.Parse(Console.ReadLine());
+
+Console.Write(anos"Digite meses: ");
+int meses = int.Parse(Console.ReadLine());
+
+Console.Write(meses"Digite dias: ");
+int dias = int.Parse(Console.ReadLine());
+
+int totalDias + anos *365 + meses * 30 + dias;
+
+Console.WriteLine($"A idade em dias é´: {totalDias}");
+
+

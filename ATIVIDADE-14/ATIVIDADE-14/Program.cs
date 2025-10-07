bda8a11b49ca57bd2e2c8a0e1453d84041c5101b
pinafomariaclara-glitch <pinafomariaclara@gmail.com>
2025-10-07 14:32:00 -0300

    Add files via upload

---
+++ ATIVIDADE-14/ATIVIDADE-14/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Digite um valor N (maior que 0): ");
+int N =.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine($"/nNUmeros de 1 ate {N} :");
+for (int i = 1; i <= N; 1++)
+
+    Console.WriteLine(1);
+
+Console.WriteLine("/nPressione Enter para sair...");
+Console.ReadLine();

26eb25641d485517e7323e30ef39d4678825c127
pinafomariaclara-glitch <pinafomariaclara@gmail.com>
2025-10-07 16:37:44 -0300

    Add files via upload

---
+++ ATIVIDADE-17/ATIVIDADE-17/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Quantos valores deseja digitar? ");
+int n = int.Parse(Console.ReadLine());
+
+int negativos = 0;
+
+for (int i = 1; i <= n; i++)
+
+    Console.Write($"Digite o {i} valor: ");
+
+int valor = int.Parse(Console.ReadLine());
+
+if (valor < 0) ;
+
+negativos ==;

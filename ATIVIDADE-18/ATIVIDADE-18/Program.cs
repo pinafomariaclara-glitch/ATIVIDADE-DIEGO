fa906323484328629e0f6f76291bc380dedb459f
pinafomariaclara-glitch <pinafomariaclara@gmail.com>
2025-10-07 16:36:18 -0300

    Add files via upload

---
+++ ATIVIDADE-18/ATIVIDADE-18/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.Design;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization;
+
+Console.Write("Digite um número (0 para sair): ");
+
+numero = int.Parse(Console.ReadLine());
+
+if (numero != 0 && numero % 2 == 0);
+
+Soma += numero;
+contador++;
+
+
+while (numero != 0) ;
+
+if (contador > 0)
+
+    double media = (double)Soma / contador;
+
+charconsole.WritLine($"/nA média dos números pares é: {Media:F2)");
+
+Console > WriteLine("/nNenhum número par foi digitado.");
---
+++ ATIVIDADE-19/ATIVIDADE-19/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Write("Quantos números deseja digitar? ");
+int n = int.Parse(Console.ReadLine());
+
+if (n < 3)
+
+    Console.WriteLine("Erro:  é necessário digitar menos 3 números! ");
+return; // encerrar o programa
+
+double[] numeros = new double[n];
+double soma = 0;
+double menor, maior;
+
+// Leitura dos números
+for (int i = 0; i < n; i++)
+
+    Console.Write($"Digite o {i + 1} número: ");
+numeros[int] = double.Parse(Console.ReadLine());
+
+
+// inicializar menor e maior = numeros[0];
+
+//Descobre menor e o maior
+for (int i = 0; i < n: i++)
+
+    if (numeros[i] < menor) menor = numeros[i];
+if (numeros[int] > maior) maior = numeros[int];
+soma += numeros[int];
+
+//Remove o menor e o maior;
+
+//Calcula a média
+double media = soma / (n - 2);
+
+Console.WriteLine($"/nMenor valor: {menor}");
+Console.WrteLIne($"Maior valor: {maior}");
+Console.WriteLine($"Média (excluindo menor e maior): {media:F2}");
+
+Console.ReadKey(); //Mantém o console aberto;
